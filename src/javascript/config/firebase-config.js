fbc7945f408fce42c07762876c3c3e5f85b086e8
Elaine Crisostomo <17244824+ecrisost@users.noreply.github.com>
2021-04-23 17:16:14 -0700

    configured firebase

--- src/javascript/config/firebase-config.js
+++ src/javascript/config/firebase-config.js
@@ -11,18 +11,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-
-$('#btnClick').click(function(){
-	db.collection("users").add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-    });
-}) 
---
+++ src/javascript/test.js
@@ -0,0 +1,16 @@
+require('config/firebase-config.js')
+var db = firebase.firestore();
+
+$('#btnClick').click(function(){
+	db.collection("users").add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}) 
